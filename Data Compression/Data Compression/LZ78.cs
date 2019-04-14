d40454f7bd3778a1310c19fe2e9365800febeb1e
Alexander <informery4@gmail.com>
2019-04-14 23:24:05 +0300

    Added LZ78

---
+++ Data Compression/Data Compression/LZ78.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Data_Compression
+{
+    class LZ78 :ITextEncodingAlgorithm
+    {
+        public string Encode(string sourceText)
+        {
+            var dictionary = new Dictionary<string, int>();
+            var answer = new StringBuilder();
+            var buffer = string.Empty;
+
+            foreach (var symbol in sourceText)
+            {
+                if (dictionary.ContainsKey(buffer + symbol))
+                {
+                    buffer += symbol;
+                }
+                else
+                {
+                    answer.Append(string.Format("({0},{1})", dictionary[buffer], symbol));
+                    dictionary.Add(buffer + symbol, dictionary.Count + 1);
+                    buffer = string.Empty;
+                }
+            }
+
+            return answer.ToString();
+        }
+
+        public string Decode(string codedText)
+        {
+            var codedTextPattern = @"\A(\(\d+,(.|\n)\))+\z";
+            if (Regex.Match(codedText, codedTextPattern).Success)
+            {
+                var singleCodePattern = @"\d+,(.|\n)";
+                var answer = new StringBuilder();
+                var dictionary = new List<string>
+                {
+                    string.Empty
+                };
+
+                foreach (Match match in Regex.Matches(codedText, singleCodePattern))
+                {
+                    var position = int.Parse(match.Value.Remove(match.Value.Length - 2));
+                    var symbol = match.Value[match.Value.Length - 1];
+                    string word = string.Format("{0}{1}", dictionary[position], symbol);
+                    answer.Append(word);
+                    dictionary.Add(word);
+                }
+
+                return answer.ToString();
+            }
+            else throw new ArgumentException();          
+        }
+    }
+}
--- Data Compression/Data Compression/RLE.cs
+++ Data Compression/Data Compression/RLE.cs
@@ -15,21 +15,21 @@ namespace Data_Compression
             if (Regex.Match(codedText, codedTextPattern).Success)
             {
                 var singleCodePattern = @"(.|\n),\d+";
-                var strBuilder = new StringBuilder();
+                var answer = new StringBuilder();
 
                 foreach (Match match in Regex.Matches(codedText, singleCodePattern))
                 {
-                    strBuilder.Append(match.Value[0], int.Parse(match.Value.Substring(2)));
+                    answer.Append(match.Value[0], int.Parse(match.Value.Substring(2)));
                 }
 
-                return strBuilder.ToString();
+                return answer.ToString();
             }
             else throw new ArgumentException();
         }
 
         public string Encode(string sourceText)
         {
-            var strBuilder = new StringBuilder();
+            var answer = new StringBuilder();
             int counter = 1;
 
             for (int i = 1; i < sourceText.Length; i++)
@@ -37,13 +37,13 @@ namespace Data_Compression
                 if (sourceText[i - 1] == sourceText[i]) counter++;
                 else
                 {
-                    strBuilder.Append(string.Format("({0},{1})", sourceText[i - 1], counter));
+                    answer.Append(string.Format("({0},{1})", sourceText[i - 1], counter));
                     counter = 1;
                 }
             }
 
-            strBuilder.Append(string.Format("({0},{1})", sourceText[sourceText.Length - 1], counter));
-            return strBuilder.ToString();
+            answer.Append(string.Format("({0},{1})", sourceText[sourceText.Length - 1], counter));
+            return answer.ToString();
 
         }
     }
